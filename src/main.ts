3b715d92cdfa1a043e05d9535bef00128645ef9e
F.LIE <mouseanone28@gmail.com>
2025-10-20 12:46:01 +0700

    add skills.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 declare const lucide: any;
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -21,20 +22,18 @@ window.addEventListener("DOMContentLoaded", () => {
     sun.classList.add("hidden");
     moon.classList.remove("hidden");
   }
+
+  initSwiper();
 });
 
-// switch mode
 const mode = localStorage.getItem("theme")
-  ? JSON.parse(localStorage.getItem("theme"))
+  ? JSON.parse(localStorage.getItem("theme")!)
   : null;
 
 const html = document.documentElement;
-
-const toggle = document.querySelector(".navbar-link-switch-mode");
-
-const sun = document.querySelector(".light-icon");
-
-const moon = document.querySelector(".dark-icon");
+const toggle = document.querySelector(".navbar-link-switch-mode")!;
+const sun = document.querySelector(".light-icon")!;
+const moon = document.querySelector(".dark-icon")!;
 
 toggle.addEventListener("click", () => {
   html.classList.toggle("dark");
@@ -42,18 +41,16 @@ toggle.addEventListener("click", () => {
   sun.classList.toggle("hidden");
   moon.classList.toggle("hidden");
 
-  if (html.className === "dark") {
-    localStorage.setItem("theme", JSON.stringify("dark"));
-  } else {
-    localStorage.setItem("theme", JSON.stringify("light"));
-  }
+  localStorage.setItem(
+    "theme",
+    JSON.stringify(html.classList.contains("dark") ? "dark" : "light")
+  );
 });
 
-// drawer
-const menu = document.querySelector(".navbar-link-icon");
-const drawer = document.querySelector(".drawer");
+const menu = document.querySelector(".navbar-link-icon")!;
+const drawer = document.querySelector(".drawer")!;
 const drawerLinkItems = document.querySelectorAll(".drawer-link a");
-const closeDrawer = document.querySelector(".close-drawer");
+const closeDrawer = document.querySelector(".close-drawer")!;
 
 drawerLinkItems.forEach((e) => {
   e.addEventListener("click", () => {
@@ -68,3 +65,125 @@ menu.addEventListener("click", () => {
 closeDrawer.addEventListener("click", () => {
   drawer.classList.remove("show");
 });
+
+function initSwiper() {
+  const SwiperGlobal = (window as any).Swiper;
+  if (!SwiperGlobal) {
+    console.error("Swiper belum dimuat dari CDN!");
+    return;
+  }
+
+  const slides = [
+    {
+      name: "HTML5",
+      image:
+        "https://img.icons8.com/?size=100&id=owWiEaAJmGKK&format=png&color=000000",
+    },
+    {
+      name: "CSS3",
+      image:
+        "https://img.icons8.com/?size=100&id=4d9YPiN04osD&format=png&color=000000",
+    },
+    {
+      name: "JavaScript",
+      image:
+        "https://img.icons8.com/?size=100&id=YX2eDFImIaWh&format=png&color=000000",
+    },
+    {
+      name: "TypeScript",
+      image:
+        "https://img.icons8.com/?size=100&id=Xf1sHBmY73hA&format=png&color=000000",
+    },
+    {
+      name: "Angular",
+      image:
+        "https://img.icons8.com/?size=100&id=6SWtW8hxZWSo&format=png&color=000000",
+    },
+    {
+      name: "React",
+      image:
+        "https://img.icons8.com/?size=100&id=asWSSTBrDlTW&format=png&color=000000",
+    },
+    {
+      name: "NextJS",
+      image:
+        "https://img.icons8.com/?size=100&id=MWiBjkuHeMVq&format=png&color=000000",
+    },
+    {
+      name: "TailwindCSS",
+      image:
+        "https://img.icons8.com/?size=100&id=x7XMNGh2vdqA&format=png&color=000000",
+    },
+    {
+      name: "Figma",
+      image:
+        "https://img.icons8.com/?size=100&id=zfHRZ6i1Wg0U&format=png&color=000000",
+    },
+    {
+      name: "Supabase",
+      image:
+        "https://img.icons8.com/?size=100&id=sH0rW2TvYdr9&format=png&color=000000",
+    },
+    {
+      name: "NodeJS",
+      image:
+        "https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000",
+    },
+    {
+      name: "Express",
+      image:
+        "https://img.icons8.com/?size=100&id=kg46nzoJrmTR&format=png&color=000000",
+    },
+    {
+      name: "Prisma",
+      image:
+        "https://img.icons8.com/?size=100&id=aqb9SdV9P8oC&format=png&color=000000",
+    },
+    {
+      name: "MongoDB",
+      image:
+        "https://img.icons8.com/?size=100&id=8rKdRqZFLurS&format=png&color=000000",
+    },
+    {
+      name: "PostgresSQL",
+      image:
+        "https://img.icons8.com/?size=100&id=JRnxU7ZWP4mi&format=png&color=000000",
+    },
+  ].map(
+    (e) =>
+      `<div class="swiper-item">
+              <div class="glass swiper-item-image-container">
+                <img src="${e.image}"
+                  class="swiper-item-image" alt="skill image">
+              </div>
+              <span class="swiper-item-image-text">${e.name}</span>
+            </div>`
+  );
+
+  const swiper = new SwiperGlobal(".swiper", {
+    slidesPerView: 6,
+    spaceBetween: 0,
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    virtual: { slides },
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+      reverseDirection: true,
+    },
+    speed: 3000,
+    breakpoints: {
+      576: { slidesPerView: 3 },
+      768: { slidesPerView: 4 },
+      992: { slidesPerView: 5 },
+      1200: { slidesPerView: 7 },
+    },
+
+    // effect: "slide",
+  });
+
+  swiper.on("slideChange", () => {
+    console.log("Slide active index:", swiper.activeIndex);
+  });
+}
